c9821ab3ebc5ab85bc2cb145a98284850d6e4eeb
Vinicius <vishoiti@gmail.com>
2026-03-31 10:38:13 -0300

    feat: Operadores aritmeticos, logicos de comparaçao

--- src/js/script.js
+++ src/js/script.js
@@ -16,29 +16,51 @@
 //taxaJuros =0.50;
 //console.log(taxaJuros);
 
-var exemplo1="Ola dev var";
-console.log(exemplo1);
+// var exemplo1="Ola dev var";
+// console.log(exemplo1);
 
-let exemplo2="Ola dev let"
-console.log(exemplo2);
+// let exemplo2="Ola dev let"
+// console.log(exemplo2);
 
-const exemplo3="Ola dev const"
-console.log(typeof exemplo3);
+// const exemplo3="Ola dev const"
+// console.log(typeof exemplo3);
 
-let exemplo4=20;
-console.log(typeof exemplo4);
+// let exemplo4=20;
+// console.log(typeof exemplo4);
 
-let exemplo5=true;
-console.log(typeof exemplo5);
+// let exemplo5=true;
+// console.log(typeof exemplo5);
 
-let exemplo6={nome:"fiap"};
-console.log(typeof exemplo6)
+// let exemplo6={nome:"fiap"};
+// console.log(typeof exemplo6)
 
-let exemplo=["Java, python, c#"];
-console.log(typeof exemplo7);
-//variavel indefinida
-let exemplo8;
-console.log(exemplo8);
-//variavel vazia
-let exemplo9=null;
-console.log(exemplo9)
+// let exemplo=["Java, python, c#"];
+// console.log(typeof exemplo);
+// //variavel indefinida
+// let exemplo8;
+// console.log(exemplo8);
+// //variavel vazia
+// let exemplo9=null;
+// console.log(exemplo9)
+
+const valor1=10;
+const valor2=20;
+
+console.log(valor1 + valor2);
+console.log(valor1 / valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+
+ const valor1=10;
+console.log(valor1 > valor2)
+console.log(valor1 > 10 && valor2 != 20);
+console.log(valor2 < 5 || valor1 > 50)
+
+// OPERADORES DE COMPARAÇÃO
+console.log(valor1 < valor2)
+console.log(valor1 <= valor2)
+console.log(valor1 >= valor2)
+console.log(valor1 != valor2)
+console.log(valor1 == valor2)
+console.log(valor1 === valor2)
+onsole.log("O valor é:", valor2)
