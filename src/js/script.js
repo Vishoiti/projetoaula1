44d254d26e27effac161dd6a5f9a503e844c873b
Vinicius <labsfiap@fiap.com.br>
2026-04-07 10:51:42 -0300

    feat: estrutura de repeticao for

--- src/js/script.js
+++ src/js/script.js
@@ -66,14 +66,109 @@
 
 // exercicio 1:
 
-const p =10;
-const v = 30;
-const PrecoFinal = p-v;
-console.log("O preço final com desconto é:", PrecoFinal)
+// const p =10;
+// const v = 30;
+// const PrecoFinal = p-v;
+// console.log("O preço final com desconto é:", PrecoFinal)
 
-// exercicio 2:
+// // exercicio 2:
 
-const login = 1;
-const token = 100;
-const VerificacaoDeSegurança = login === 1  && token >=10;
-console.log("Verificaçao autorizada com sucesso:", VerificacaoDeSegurança);
+// const login = 1;
+// const token = 100;
+// const VerificacaoDeSegurança = login === 1  && token >=10;
+// console.log("Verificaçao autorizada com sucesso:", VerificacaoDeSegurança);
+
+// ESTRUTURA CONDICIONAL
+//if
+// if(true){
+//     console.log("é Verdadeiro")
+// }
+
+// //if/else
+
+// let nome= "Fiap";
+
+// if(nome=="Fiap"){
+//     console.log("nome verdadeiro")
+//     }else{
+//         console.log("nome Errado")
+//     }
+
+//     //if encadeado ou animado
+
+//     let idade =17;
+    
+//     if(idade <=13){
+//         console.log("é uma criança")
+//     }else if(idade >13 && idade <=18){
+//         console.log("é um adolescente")
+//     }else if(idade >18 && idade <-59)
+//         console.log("é um adulto")
+//     else{
+//         console.log("é um idoso")
+//     }
+
+//     //SWITCH CASE
+
+//     let time="santos";
+    
+//     switch(time){
+//         case "santos":
+//             console.log("Um time idoso")
+//             break;
+//         case "sao paulo":
+//             console.log("Time Foda")
+//             break;
+//         case "palmeiras":
+//             console.log("SEM MUNDIAL")
+//             break;
+//         case "corinthians":
+//             console.log("Time de pateta")
+//             break;
+//         default:
+//             console.log("é tão ruim quanto a seleção brasileira")
+//     }
+
+// // ternaria
+
+// let salario=100;
+// let resultado = salario ==100 ? "Salario Certo":"SalrioErrado"
+// console.log(resultado)
+
+// let usuario="fiap";
+// let userLogado= usuario ==="fiap"?"Logado com sucesso":!"usuario invalido"
+// console.log(userLogado);
+
+// function verificarParImpar(numero){
+//     return numero % 2 === 0 ?"PAR":"IMPAR";
+// }
+// console.log(verificarParImpar(10))
+
+// ESTRUTURA DE REPETIÇÃO (LAÇO DE REPETIÇÃO)
+
+// for - Estrutura de repetição mais utilizado quando sabemos previamente quantas vezes
+// o código vai ser executado.
+
+//declaração, operação e incremento
+for(let i=1; i<=5;i++){
+    console.log("Numer:",i);
+}
+// for para array   (ESTUDAR FOR E IN NO JAVA SCRIPT, PARA SABER PARTE DE DENIMINAÇÃO DE DADOS E ETC...)
+
+const linguagens = ["Java", "Python", "C#"];
+
+for(let i=0; i < linguagens.length;i++){
+    console.log("Linguagens",i ,"i", linguagens[i]);
+}
+
+const tecnologias =["JS", "HTML", "CSS"]
+
+for(const tech of tecnologias){
+    console.log("Estudando", tech)
+}
+
+const carros ={marca:"Volks", modelo:"Fusca", ano:1980};
+
+for(let dados in carros){
+    console.log(dados, ":", carros[dados])
+}
